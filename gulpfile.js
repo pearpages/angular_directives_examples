eee58f77732df6ecd8c83cdebe5f97e1bb5be3fb
pearpages <pere@soms.cat>
2015-11-22 19:46:11 +0100

    html injection

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,24 @@
     var args = require('yargs').argv;
     var $ = require('gulp-load-plugins')({lazy: true});
 
+    var js = [
+            'app/**/*.module.js',
+            'app/**/*.js',
+            '!app/**/*.spec.js'
+        ];
+
+    gulp.task('wiredep', function() {
+        var options = {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+        };
+
+        var wiredep = require('wiredep').stream; //.stream is to use it with gulp
+
+        return gulp
+            .src('index.html')
+            .pipe(wiredep(options))
+            //.pipe($.inject(gulp.src(js)))
+            .pipe(gulp.dest('./'));
+    });
 }());
